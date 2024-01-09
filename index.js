18486b555f0f3b5ff5a83ac9f761f2ac3137f6eb
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-09 03:49:52 -0800

    adding the logic of it

--- index.js
+++ index.js
@@ -203,7 +203,17 @@ function animate() {
   }
 
   // end game based on health
-  
+  if (enemy.health <= 0 || player.health <= 0) {
+    document.querySelector("#displayText").style.display = "flex";
+
+    if (player.health == enemy.health) {
+      document.querySelector("#displayText").innerHTML = "Tie";
+    } else if (player.health > enemy.health) {
+      document.querySelector("#displayText").innerHTML = "Player 1 Wins";
+    } else if (player.health < enemy.health) {
+      document.querySelector("#displayText").innerHTML = "Player 2 Wins";
+    }
+  }
 }
 
 animate();
