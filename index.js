46bbfee427a180b969494b81f212e6100ecb5f24
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-09 03:58:39 -0800

    adding the determine function into the health bar

--- index.js
+++ index.js
@@ -207,16 +207,9 @@ function animate() {
 
   // end game based on health
   if (enemy.health <= 0 || player.health <= 0) {
-    document.querySelector("#displayText").style.display = "flex";
+    determineWinner({player, enemy})
+  }
 
-    if (player.health == enemy.health) {
-      document.querySelector("#displayText").innerHTML = "Tie";
-    } else if (player.health > enemy.health) {
-      document.querySelector("#displayText").innerHTML = "Player 1 Wins";
-    } else if (player.health < enemy.health) {
-      document.querySelector("#displayText").innerHTML = "Player 2 Wins";
-    }
-  } 
 }
 
 animate();
