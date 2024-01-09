e3f2d2e6087bb37d441b1a46b0acef4165ad672d
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-09 04:09:45 -0800

    cancelling the timeout

--- index.js
+++ index.js
@@ -131,6 +131,7 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
 }
 
 function determineWinner({player, enemy, timeId}){
+  cancelTimeout(timeId)
   document.querySelector("#displayText").style.display = "flex";
   if (player.health == enemy.health) {
     document.querySelector("#displayText").innerHTML = "Tie";
